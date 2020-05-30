15afa38f5afdfb3fbd06f6042b4de49374747ec5
Davide Rasoli <rasoli.davide@iisgalvanimi.edu.it>
2020-05-30 23:53:54 +0000

    Rasoli e Giuliani: Funziona interprete voto ffino a data

--- angular-revocale-D5/src/app/inserimento-vocale-con-riepilogo-component/inserimento-vocale-con-riepilogo-component.component.ts
+++ angular-revocale-D5/src/app/inserimento-vocale-con-riepilogo-component/inserimento-vocale-con-riepilogo-component.component.ts
@@ -5,7 +5,7 @@ import { OnDestroy } from '@angular/core';
 import { AudioRecordingService } from './audio-recording.service';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-
+import { Voti } from '../voti.model';
 
 @Component({
   selector: 'app-inserimento-vocale-con-riepilogo-component',
@@ -67,31 +67,106 @@ export class InserimentoVocaleConRiepilogoComponentComponent implements OnInit, 
   ngOnDestroy(): void {
     this.abortRecording();
   }
-  findKeywordsIndexes(transcriptionArray : Array<String>, keywords : Array<String>) : Array<number>
+  findKeywordsIndexes(transcriptionArray : Array<String>, keywords : Array<String>) : Array<Object>
   {
-    let indexes = new Array<number>();
+    let indexes = new Array<Object>();
     for(let i = 0; i < keywords.length; i++)
     {
       let foundIndex = transcriptionArray.indexOf(keywords[i]);
-      indexes.push(foundIndex);
+      console.log(foundIndex, keywords[i]);
+      if(foundIndex != -1)
+      {
+        let corr = {"keyword" : keywords[i], "index" : foundIndex};
+        indexes.push(corr);
+      }
     }
     return indexes;
   }
   readVoto(transcription : string)
   {
-    let keywordsVoto = ["Voto", "Studente", "Descrizione", "Data", "Peso"];
+    let keywordsVoto = ["voto", "studente", "descrizione", "data", "peso", "valore"];
     let transcriptionArray = transcription.split(" ");
+    console.log(transcriptionArray);
+    let corrispondenze = this.findKeywordsIndexes(transcriptionArray, keywordsVoto);
+    console.log(corrispondenze);
+    let voto, studente = "", descrizione = "", data, peso, valore;
+    let votoOgg = new Voti();
+    let corrispondenzaVoto = corrispondenze.find((corrispondenza) =>
+    {
+      return corrispondenza["keyword"] == "voto";
+    });
 
-    let indexVoto = transcriptionArray.indexOf('voto');
-    let indexStudente = transcriptionArray.indexOf('studente');
-    let indexDescrizione = transcriptionArray.indexOf('descrizione');
+    if(corrispondenzaVoto == null)
+    {
+      alert("Voto non trovato nel comando vocale");
+      return;
+    }else
+      voto =  0 + Number(transcriptionArray[corrispondenzaVoto['index'] + 1]);
 
-    let voto = transcriptionArray[indexVoto + 1];
-    let studente = transcriptionArray[indexStudente + 1] + " " + transcriptionArray[indexStudente + 2];
-    let descrizione = transcriptionArray.slice(indexDescrizione + 1);
-    console.log("Voto: " + voto, indexVoto);
-    console.log("Studente: " + studente, indexStudente);
-    console.log("Descrizione: " + descrizione.toString().replace(",", " "), indexDescrizione);
+    if(String(voto) == "NaN")
+    {
+      alert("Il voto non è un numero");
+      return;
+    }
+    votoOgg.Voto = voto;
+    let corrispondenzaStudente = corrispondenze.find((corrispondenza) =>
+    {
+      return corrispondenza["keyword"] == "studente";
+    });
+
+    let corrispondenzaDescrizione = corrispondenze.find((corrispondenza) =>
+    {
+      return corrispondenza["keyword"] == "descrizione";
+    });
+    console.log(corrispondenzaStudente);
+    console.log(corrispondenzaDescrizione);
+
+    if(corrispondenzaStudente == null || corrispondenzaDescrizione == null)
+    {
+      alert("Dati mancanti");
+      return;
+    }
+    else
+    {
+      for(let i = (corrispondenzaStudente['index'] + 1); i < corrispondenzaDescrizione['index']; i++)
+        studente += transcriptionArray[i] + " ";
+
+      studente = studente.trim();
+    }
+
+    let nextCorrispondenza = corrispondenze[3];
+    if(nextCorrispondenza == undefined)
+    {
+      descrizione = transcriptionArray.slice(corrispondenzaDescrizione['index'] + 1).toString().replace(/,/g, " ").replace("\"", " ");
+    }
+    else
+    {
+      for(let i = corrispondenzaDescrizione['index'] + 1; i < nextCorrispondenza['index']; i++)
+        descrizione += transcriptionArray[i] + " ";
+
+      descrizione = descrizione.trim();
+    }
+    votoOgg.Descrizione = descrizione;
+    let corrispondenzaData = corrispondenze.find((corrispondenza) =>
+    {
+      return corrispondenza["keyword"] == "data";
+    });
+
+    if(corrispondenzaData != null)
+    {
+      let mesianno =  ['gennaio', 'febbraio', 'marzo', 'aprile', 'maggio', 'giugno', 'luglio', 'agosto', 'settembre', 'ottobre', 'novembre', 'dicembre'];
+      let giorno = Number(transcriptionArray[corrispondenzaData['index'] + 1]);
+      let mese = mesianno.indexOf(transcriptionArray[corrispondenzaData['index'] + 2]);
+      let anno = Number(transcriptionArray[corrispondenzaData['index'] + 3]);
+      console.log(giorno, mese, anno)
+      data = new Date(anno, mese, giorno);
+    }
+    else
+    {
+      data = new Date();
+    }
+    votoOgg.DataVoto = data;
+    console.log(votoOgg, studente);
   }
   readNota(transcription : string)
   {
@@ -133,20 +208,17 @@ export class InserimentoVocaleConRiepilogoComponentComponent implements OnInit, 
   }
   receiveTranscription = (transcription : string) =>
   {
-    transcription = String(transcription.replace("\n", " ")).trim();
-    console.log("Received transcription: " + transcription);
+    transcription = String(transcription.replace(/\n/g, " ").replace(/\"/g, " ")).trim();
     console.log(typeof transcription);
-    this.lasttranscription = transcription;
     transcription = transcription.toLowerCase();
+    this.lasttranscription = transcription;
+
     //Inserimento vocale voto: Inserisci voto <1 => 10> a studente <Nome Cognome Studente> Descrizione <Descrizione> [Data <Giorno - Mese - Anno> Peso <0 - 100> {Con Valore | Senza Valore | Recupero}]
-    console.log(transcription, typeof transcription);
     if(!transcription.includes("Inserisci voto"))
       this.readVoto(transcription);
-    if(!transcription.includes("Inserisci nota"))
+    else if(!transcription.includes("Inserisci nota"))
       this.readNota(transcription);
-    if(!transcription.includes("Inserisci assenza"))
-      this.readVoto(transcription);
+    else if(!transcription.includes("Inserisci assenza"))
+      this.readAssenza(transcription);
   }
-
-
 }
